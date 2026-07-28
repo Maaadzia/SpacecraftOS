3122178d65e3e5a29f280f406ff72362336e42ff
Maaadzia <madzika.beza@gmail.com>
2026-07-28 14:01:03 +0200

    ISS catch guide window

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function updateTime() {
     setInterval(updateTime, 1000);
 
 
+dragElement(document.getElementById("issGuide"));
+//OPEN AND CLOSE WINDOW ISS GUIDE
+
+var issGuideScreen = document.querySelector("#issGuide")
+
+function closeWindow(element) {
+  element.style.display = "none"
+}
+
+function openWindow(element) {
+  element.style.display = "flex"
+}
+
+
+var issGuideScreenClose = document.querySelector("#issGuideclose")
+
+var issGuideScreenOpen = document.querySelector("#issGuideopen")
+
+
+issGuideScreenClose.addEventListener("click", function() {
+  closeWindow(issGuideScreen);
+});
+issGuideScreenOpen.addEventListener("click", function() {
+  openWindow(issGuideScreen);
+});
