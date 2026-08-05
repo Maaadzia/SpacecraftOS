c834346ae474ab562e8e9929397516ae8c167d84
Maaadzia <madzika.beza@gmail.com>
2026-08-05 20:37:11 +0200

    Deleted code from tutorial
    Unfortunately, my code was 1:1 like in the tutorial and it was not working. I decided to just create new repository and copy paste code in it - it worked. I was looking for the error in my code but I haven't found anything so I gave up and got back to what I had before this tutorial. I'm gonna look for sth else!

--- script.js
+++ script.js
@@ -269,30 +269,3 @@ initializeWindow("HubbleSpaceTelescopeApp")
 initializeWindow("ArtemisApp")
 
 
-
-
-
-//SLS IMAGES SLIDER
-
-const slides = document.querySelectorAll(".slides img");
-let slideIndex = 0;
-let intervalId = null;
-
-initializeSlider();
-
-function initializeSlider(){
-
-  slides[slideIndex].classList.add("displaySlide");
-  intervalId = setInterval(nextSlide, 5000);
-  console.log(intervalId)
-
-}
-function showSlide(index){
-
-}
-function prevSlide(){
-
-}
-function nextSlide(){
-
-}
