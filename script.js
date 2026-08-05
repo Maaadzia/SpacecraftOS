831bc4f72fb40f96818682c8bf18041cb76edf3a
Maaadzia <madzika.beza@gmail.com>
2026-08-05 20:07:39 +0200

    Imagess slider from tutorial - not working
    looking for mistake 😭😭

--- script.js
+++ script.js
@@ -268,3 +268,31 @@ initializeWindow("NASCApp")
 initializeWindow("HubbleSpaceTelescopeApp")
 initializeWindow("ArtemisApp")
 
+
+
+
+
+//SLS IMAGES SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+initializeSlider();
+
+function initializeSlider(){
+
+  slides[slideIndex].classList.add("displaySlide");
+  intervalId = setInterval(nextSlide, 5000);
+  console.log(intervalId)
+
+}
+function showSlide(index){
+
+}
+function prevSlide(){
+
+}
+function nextSlide(){
+
+}
